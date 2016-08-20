1e51d6c4aaf3ad0082f61adfcaa0377e87fd717b
KinYee <jocelyn.ky.lee@gmail.com>
2016-08-20 01:49:53 -0700

    changed vars minage & maxage to min & max

--- signup.php
+++ signup.php
@@ -13,30 +13,31 @@
 	
 <div id="main">
 <form action="signup-submit.php" method="post">
+<!--input names: name, gender, age, type, comp, min, max-->
 Name:
 <input name="name" type="text" size="16">
 <br>
 Gender (Sorry this is a hetero sight):
-<input type="radio" name="gender" value="male" checked>Male
-<input type="radio" name="gender" value="female">Female
+<input name="gender" type="radio" value="male" checked>Male
+<input name="gender" type="radio" value="female">Female
 <br>
 Age:
-<input type="number" name="age" min="18" max="150">
+<input name="age" type="number" min="18" max="150">
 <br>
 Personality type:
-<input type="text" name="keirsey" size="6" maxlength="4">
+<input name="type" type="text" size="6" maxlength="4">
 (<a href="http://www.humanmetrics.com/cgi-win/JTypes2.asp">Don't know your type?</a>)
 <br>
 Favorite OS:
-<select name="comp">
+<select name="comp"> <!-- snooty shit: who uses just one OS for coding though... -->
 	<option value="windows" selected>Windows</option>
 	<option value="mac">Mac OS X</option>
 	<option value="linux">Linux</option>
 </select>
 <br>
 Seeking age:
-<input name="minage" type="number" min="18" max="150">
- to <input name="maxage" type="number" min="18" max="150">
+<input name="min" type="number" min="18" max="150">
+ to <input name="max" type="number" min="18" max="150">
 <br>
 <input type="submit" value="Sign Up" >
 </form>
