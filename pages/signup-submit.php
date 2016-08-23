bb8716380102174b8b681f8f50f4c75de142b3bc
KinYee <jocelyn.ky.lee@gmail.com>
2016-08-22 19:21:30 -0700

    Update signup-submit.php

--- pages/signup-submit.php
+++ pages/signup-submit.php
@@ -1,42 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>NerdLuv - Signup Submitted</title>
-<link href="../css/nerdluv.css" rel="stylesheet" >
-<meta charset="utf-8" >
-</head>
-<body>
-<!-- include header -->
+<?php
+include("top.html");
+include("constants.php");
+
+function isNotType($type) {
+	return	!(($type[0] == "E" || $type[0] == "I") &&
+			($type[1] == "N" || $type[1] == "S") &&
+			($type[2] == "F" || $type[2] == "T") &&
+			($type[3] == "J" || $type[3] == "P"));
+}
+?>
+<!-- page after signup info is submitted -->
 
 <div id="main">
 <h1>Thank you!</h1>
 <br><br>
 <?php
 //input names: name, gender, age, type, comp, min, max
-$name = $_POST["name"];
-$gender = $_POST["gender"];
-$age = $_POST["age"];
-$type = $_POST["type"];
-$comp = $_POST["comp"];
-$min = $_POST["min"];
-$max = $_POST["max"];
-if ($min >= $max) die("dont be naughty, give a valid age range");
-//screw testing for kiersey type validity bc im stick my middle fingers up at these prep tests
+$user = array(	$_POST["name"],
+				$_POST["gender"],
+				$_POST["age"],
+				$_POST["type"],
+				$_POST["comp"],
+				$_POST["min"],
+				$_POST["max"]);
+if ($user[$MIN] >= $user[$MAX]) die("dont be naughty, give a valid age range");
+if (isNotType($user[$TYPE])) die("look i don't like keirsey either but u gotta");
 
-//i'd sorta rather use a for loop but... ehhh
-$user = $name.",".$gender.",".$age.",".$type.",".$comp.",".$min.",".$max;
+$entry = implode(",", $user);
+
 try {
-	file_put_contents(__DIR__."/../singles.txt", $user);
-	echo "<p>Welcome to NerdLuv, ".$name."</p>";
-} catch (Exception $e) {
-	echo "<p>no database fer u u jerk</p>"
-}
+	file_put_contents(__DIR__."/../singles.txt", $entry, FILE_APPEND);
 ?>
+	<p>Welcome to NerdLuv, <?= $name ?></p>
+<?php } catch (Exception $e) { ?>
+	<p>no database fer u u jerk</p>
+<?php } ?>
 
 <br><br>
 Now <a href="matches.php">log in to see your matches!</a>
-
 </div>
 
-<!-- include footer -->
-</html>
+<?php include("bottom.html"); ?>
