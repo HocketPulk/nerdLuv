c060032d0321b32407941fc997e07fd3060db0a7
KinYee <jocelyn.ky.lee@gmail.com>
2016-08-25 19:01:07 -0700

    Delete show.js

--- show.js
+++
@@ -1,9 +0,0 @@
-function showSignup() {
-	document.getElementById("option2").style.display = "none"
-	document.getElementById("option1").style.display = "block"
-}
-
-function showMatches() {
-	document.getElementById("option1").style.display = "none"
-	document.getElementById("option2").style.display = "block"
-}
