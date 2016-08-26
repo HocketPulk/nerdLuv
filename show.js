3f3b6dabcc1967ad6a44b285774f50143420f948
KinYee <jocelyn.ky.lee@gmail.com>
2016-08-25 19:21:15 -0700

    shhhh you didn't see anything

--- show.js
+++ show.js
@@ -1,9 +1,9 @@
 function showSignup() {
-	document.getElementById("option2").style.display = "none"
-	document.getElementById("option1").style.display = "block"
+	document.getElementById("option2").style.display = "none";
+	document.getElementById("option1").style.display = "block";
 }
 
 function showMatches() {
-	document.getElementById("option1").style.display = "none"
-	document.getElementById("option2").style.display = "block"
+	document.getElementById("option1").style.display = "none";
+	document.getElementById("option2").style.display = "block";
 }
