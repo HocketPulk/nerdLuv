be4f1757da4c8b10a394aeaa26867040f4f135f9
KinYee <jocelyn.ky.lee@gmail.com>
2016-08-25 19:02:02 -0700

    final version

---
+++ show.js
@@ -0,0 +1,9 @@
+function showSignup() {
+	document.getElementById("option2").style.display = "none"
+	document.getElementById("option1").style.display = "block"
+}
+
+function showMatches() {
+	document.getElementById("option1").style.display = "none"
+	document.getElementById("option2").style.display = "block"
+}
